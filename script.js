a8e4bcb707838a7cb36e2f83cad00061ddfe9f6d
Mark Cody <mac110@ads.northwestern.edu>
2017-01-03 21:41:28 -0600

    working set to firebase

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
     messagingSenderId: "193483804810"
   };
   firebase.initializeApp(config);
-</script>
 
 // Create a variable to reference the database
 var database = firebase.database();
@@ -19,37 +18,42 @@ var cardFront = '',
 
 //create class for the parent flashcard object
 class FlashCard {
-    constructor (question, answer){
+    constructor (type, question, answer){
+        this.type = type;
         this.question = question;
         this.answer = answer;
+    }
+    //write card to database
+    writeCard(type, question, answer) {
+        firebase.database().ref().set({
+        cardType: type,
+        cardFront: question,
+        cardBack: answer
+      });
     }
 }
 //create subclass for the basic card
 class BasicFlashcard extends FlashCard {
 	constructor(){
-        super(question, answer)
+        super()
     }
 };
 //create subclass for the clozecards
 class ClozeFlashcard extends FlashCard {
 	constructor(){
-        super(question, answer)
+        super()
     }
 };
 //create and save new instance using event listener.
-$("#submitCard").on("click", function() {
+/*$("#submitCard").on("click", function() {
     //store the inputs in memory
     cardType = $('#card-type').val();
     cardFront = $('#card-front').val().trim();
-	cardBack = $('#card-back').val().trim());
+	cardBack = $('#card-back').val().trim();
     //save the new card in Firebase
-    database.ref().set({
-			cardFront: cardFront,
-			cardBack: cardBack,
-            cardType: cardType
-		});
-    
-}
-//backend must be able to:
-//1. create new flashcard
-//2. save new flashcard in both formats.
+    FlashCard.writeCard(cardType, cardFront, cardBack);
+    };
+*/                   
+var test = new FlashCard('basic', 'What is a type of rock formed by sediment?', 'Sedimentary');
+console.log(test);
+test.writeCard();
